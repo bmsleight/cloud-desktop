ed39c6f91d301b1d7bd4f2260983c32bb98c170d
bms@barwap.com <bms@barwap.com@f417664d-549e-e025-d39b-a7d46fc263f0>
2010-12-16 19:55:18 +0000

    Tweaks

--- install/startvnc.sh
+++ install/startvnc.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 su cloud -c "Xvfb :1 -cc 4 -screen 0 1024x768x16 &"
 sleep 5
-x11vnc -display :1 -ssl SAVE -httpdir /usr/share/x11vnc/classes/ssl/ -httpsredir -rfbport 443 -forever -unixpw cloud >/tmp/xll.txt 2>&1 &
+# -listen ipaddr 
+x11vnc -display :1 -ssl SAVE -httpdir /usr/share/x11vnc/classes/ssl/ -httpsredir -rfbport 443 -forever -shared -unixpw cloud >/tmp/xll.txt 2>&1 &
 su cloud -c "export DISPLAY=:1 ; startxfce4"
 
 
