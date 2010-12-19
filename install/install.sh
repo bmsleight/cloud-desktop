6e43a9c0105add137d1ae36709bcfa88b4b1ea1b
bms@barwap.com <bms@barwap.com@f417664d-549e-e025-d39b-a7d46fc263f0>
2010-12-19 23:43:09 +0000

    Work in progress (5)

--- install/install.sh
+++ install/install.sh
@@ -16,10 +16,10 @@ apt-get --assume-yes install xfdesktop4 xfce4-utils xvfb x11vnc apache2-mpm-pref
 
 
 #Packages I like
-apt-get --assume-yes install abiword ace-of-penguins anacron audacious audacious-plugins chromium-browser chromium-browser-l10n desktop-file-utils evince file-roller galculator gnome-system-tools gnumeric gpicview jockey-gtk language-selector leafpad logrotate mtpaint osmo pidgin scrot simple-scan software-properties-gtk sylpheed sylpheed-i18n synaptic transmission ttf-liberation ttf-ubuntu-font-family ttf-unfonts-core ttf-wqy-microhei unzip update-notifier x11-utils xchat xdg-user-dirs xpad xscreensaver zip 
+apt-get --assume-yes install abiword ace-of-penguins anacron audacious audacious-plugins chromium-browser chromium-browser-l10n desktop-file-utils evince file-roller galculator gnome-system-tools gnumeric gpicview jockey-gtk language-selector leafpad logrotate mtpaint osmo pidgin scrot simple-scan software-properties-gtk sylpheed sylpheed-i18n synaptic transmission ttf-liberation ttf-unfonts-core ttf-wqy-microhei unzip update-notifier x11-utils xchat xdg-user-dirs xpad xscreensaver zip 
 
 # Just for chromium-browser
-chmod 777 /tmp/shm
+chmod 777 /dev/shm
 
 ############################################################
 # add_user_cloud and setup_x11vnc
@@ -45,7 +45,7 @@ mv /var/www/ajaxplorer-core-3.1.1 /var/www/file
 # username for ajaxplorer is admin, password as asked at start of script. 
 # alas this is stored in plain text.
 cat /var/www/file/server/conf/conf.php | sed s/"admin"/"$PASSWORD_USER"/ >/tmp/conf.php
-mv /tmp/conf.php /var/www/file/server/conf/conf.php
+mv -f /tmp/conf.php /var/www/file/server/conf/conf.php
 mkdir /var/www/file/public
 chown www-data.www-data /var/www/file -R
 
@@ -70,13 +70,16 @@ a2enmod ssl rewrite proxy proxy proxy_html proxy_http headers
 
 # We only want https - We may be using any network. So force https.
 echo "<VirtualHost *:80>
-> <Location / >
->         RewriteEngine   on
->         RewriteCond     %{SERVER_PORT} ^80$
->         RewriteRule (.*) https://%{HTTP_HOST}%{REQUEST_URI}
-> </Location>
-> </VirtualHost>" >/etc/apache2/sites-available/default
+<Location / >
+         RewriteEngine   on
+         RewriteCond     %{SERVER_PORT} ^80$
+         RewriteRule (.*) https://%{HTTP_HOST}%{REQUEST_URI}
+</Location>
+</VirtualHost>" >/etc/apache2/sites-available/default
 
+wget "http://cloud-desktop.googlecode.com/svn-history/trunk/install/default-ssl" -O /etc/apache2/sites-available/default-ssl
+cat /etc/apache2/ports.conf | sed s/443/127.0.0.1:443/ >/tmp/ports.conf
+mv -f /tmp/ports.conf /etc/apache2/ports.conf
 a2ensite default-ssl
 
 
@@ -84,12 +87,12 @@ a2ensite default-ssl
 # So we use the wonderful connect_switch to look at the first part of the data
 # is it "CONNECT..." if so send it to x11vnc
 # if it is not we send to apache...
-ip=`grep address /etc/network/interfaces | grep -v 127.0.0.1 | grep -v ::1 | awk '{print $2}'`
+ip=`grep address /etc/network/interfaces | grep -v 127.0.0.1 | grep -v ::1 | head -n 1 | awk '{print $2}'`
 
 wget "http://www.karlrunge.com/x11vnc/connect_switch" -O /root/connect_switch.sh
 chmod +x /root/connect_switch.sh
 echo "#!/bin/sh
-export CONNECT_SWITCH_LISTEN=$IP:443
+export CONNECT_SWITCH_LISTEN=$ip:443
 export CONNECT_SWITCH_HTTPD=127.0.0.1:443
 export CONNECT_SWITCH_ALLOWED=localhost:5900
 export CONNECT_SWITCH_LOOP=1
@@ -97,7 +100,9 @@ export CONNECT_SWITCH_LOOP=BG
 
 /root/connect_switch" >/root/start_connect_switch.sh
 chmod +x /root/start_connect_switch.sh
-
+wget "http://www.karlrunge.com/x11vnc/connect_switch" -O /root/connect_switch
+chmod +x /root/connect_switch
+/etc/init.d/apache2 restart
 
 ############################################################
 # Open VPN
--- install/startvnc.sh
+++ install/startvnc.sh
@@ -3,7 +3,7 @@
 su cloud -c "Xvfb :1 -cc 4 -screen 0 1024x768x16 &"
 sleep 5
 # -listen ipaddr 
-x11vnc -display :1 -ssl SAVE -httpdir /usr/share/x11vnc/classes/ssl/ -httpsredir -rfbport 443 -forever -shared -unixpw cloud >/tmp/xll.txt 2>&1 &
+x11vnc -display :1 -ssl SAVE -httpdir /usr/share/x11vnc/classes/ssl/ -httpsredir  -forever -unixpw cloud >/tmp/xll.txt 2>&1 &
 su cloud -c "export DISPLAY=:1 ; startxfce4"
 
 
